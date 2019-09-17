cd19406d6429d1ac4849f9f76e4362ed8b68d008
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-09-16 21:45:51 -0400

    Added uwapi importer

---
+++ backend/uwapi-importer/go.mod
@@ -0,0 +1,10 @@
+module github.com/AyushK1/uwflow2.0/backend/uwapi-importer
+
+go 1.12
+
+require (
+	github.com/jackc/pgx v3.6.0+incompatible
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
---
+++ backend/uwapi-importer/util/util.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+  "fmt"
+  "strconv"
+  "strings"
+)
+
+func TermNameToId(name string) (int, error) {
+  components := strings.Split(name, " ")
+  if len(components) != 2 {
+    return 0, fmt.Errorf("not a term name: %s", name)
+  }
+	var month int
+	switch components[0] {
+	case "Fall":
+		month = 9
+	case "Spring":
+		month = 5
+	case "Winter":
+		month = 1
+	default:
+    return 0, fmt.Errorf("not a season: %s", components[0])
+	}
+	year, err := strconv.Atoi(components[1])
+	if err != nil {
+		return 0, fmt.Errorf("not a year: %s", components[1])
+	}
+	return (year-1900)*10 + month, nil
+}
