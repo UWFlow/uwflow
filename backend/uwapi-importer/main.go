2fd1df3dc4b2a6404e4dc7a6905a74a1673b6be1
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-09-29 21:14:22 -0400

    Add selection of part

--- backend/uwapi-importer/main.go
+++ backend/uwapi-importer/main.go
@@ -3,21 +3,42 @@ package main
 import (
 	"context"
 	"log"
+  "os"
 
 	"github.com/AyushK1/uwflow2.0/backend/uwapi-importer/parts/course"
-	//"github.com/AyushK1/uwflow2.0/backend/uwapi-importer/parts/term"
+	"github.com/AyushK1/uwflow2.0/backend/uwapi-importer/parts/section"
+	"github.com/AyushK1/uwflow2.0/backend/uwapi-importer/parts/term"
 	"github.com/AyushK1/uwflow2.0/backend/uwapi-importer/state"
 )
 
-func main() {
+type ImportFunc func(*state.State) error
+
+func Run(importer ImportFunc) {
 	// TODO: set sane time limit?
 	ctx := context.Background()
 	state, err := state.New(ctx)
 	if err != nil {
 		log.Fatalf("Initialization failed: %v\n", err)
 	}
-	err = course.ImportAll(state)
+  err = importer(state)
 	if err != nil {
-		log.Fatalf("UW API import failed: %v\n", err)
+		log.Fatalf("API import failed: %v\n", err)
 	}
+}
+
+func main() {
+  if len(os.Args) != 2 {
+    log.Fatalf("Usage: %s ACTION", os.Args[0])
+  }
+
+  switch os.Args[1] {
+  case "courses":
+    Run(course.ImportAll)
+  case "sections":
+    Run(section.ImportAll)
+  case "terms":
+    Run(term.ImportAll)
+  default:
+    log.Fatalf("Not an action: %s", os.Args[1])
+  }
 }
---
+++ backend/uwapi-importer/parts/section/section.go
@@ -0,0 +1,7 @@
+package section
+
+import "github.com/AyushK1/uwflow2.0/backend/uwapi-importer/state"
+
+func ImportAll(state *state.State) error {
+  return nil
+}
