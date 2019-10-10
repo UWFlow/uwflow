08e0417e18b78d9143197f484eb5bf871c1ae9f0
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-10-09 21:32:49 -0400

    Correct webcal generation

--- backend/api/webcal/webcal.go
+++ backend/api/webcal/webcal.go
@@ -17,6 +17,7 @@ type PostgresEvent struct {
 	CourseCode   string
 	SectionName  string
 	CourseName   string
+  IsExam       bool
 	Location     *string
 	StartDate    time.Time
 	EndDate      time.Time
@@ -83,24 +84,24 @@ func WriteCalendar(w io.Writer, events []*WebcalEvent) {
 const SelectEventQuery = `
 WITH src AS (
   SELECT
-    section_id, location, start_date, end_date,
+    FALSE as is_exam, section_id, location, start_date, end_date,
     start_seconds, end_seconds, days
   FROM section_meeting
   UNION ALL
   SELECT
-    section_id, location, date, date,
+    TRUE AS is_exam, section_id, location, date, date,
     start_seconds, end_seconds, ARRAY[day]
-  FROM course_exam
+  FROM section_exam
 )
 SELECT
-  c.code, cs.section, c.name, src.location,
+  c.code, cs.section, c.name, src.is_exam, src.location,
   src.start_date, src.end_date, src.start_seconds, src.end_seconds, src.days
 FROM
   user_schedule us
   JOIN course_section cs ON cs.id = us.section_id
   JOIN src ON src.section_id = us.section_id
   JOIN course c ON c.id = cs.course_id
-WHERE us.user_id = 332
+WHERE us.user_id = $1
 	-- Fetch meetings where start_seconds and end_seconds are present.
 	-- Otherwise, we cannot say anything useful about when they take place.
   AND src.start_seconds IS NOT NULL
@@ -119,7 +120,7 @@ func ExtractUserEvents(state *state.State, userId int) ([]*PostgresEvent, error)
 		var ev PostgresEvent
 
 		err = rows.Scan(
-			&ev.CourseCode, &ev.SectionName, &ev.CourseName, &ev.Location,
+			&ev.CourseCode, &ev.SectionName, &ev.CourseName, &ev.IsExam, &ev.Location,
 			&ev.StartDate, &ev.EndDate, &ev.StartSeconds, &ev.EndSeconds, &ev.Days,
 		)
 		if err != nil {
@@ -146,6 +147,9 @@ func PostgresToWebcalEvent(event *PostgresEvent, date time.Time) *WebcalEvent {
 		"%s - %s - %s",
 		strings.ToUpper(event.CourseCode), event.SectionName, event.CourseName,
 	)
+  if event.IsExam {
+    summary = "Final exam for " + summary
+  }
 	return &WebcalEvent{
 		Summary:   summary,
 		StartTime: date.Add(time.Second * time.Duration(event.StartSeconds)),
