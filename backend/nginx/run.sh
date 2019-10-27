62f2303c2ac5a38af54d946e926bff3c5a787e2d
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-10-26 20:30:14 -0400

    Remove Letsencrypt signing in favor of Cloudflare

--- backend/nginx/run.sh
+++ backend/nginx/run.sh
@@ -5,15 +5,12 @@ set -eu
 RUNDIR="$(dirname $0)"
 VARS='$API_PORT $DOMAIN $HASURA_PORT $NGINX_HTTP_PORT $NGINX_HTTPS_PORT'
 
-# Substitute only the variables listed above. Why?
-# Nginx uses internal variables of the form $host. They must not be substituted.
-envsubst "$VARS" < "$RUNDIR/nginx.template" > /etc/nginx/nginx.conf
-envsubst "$VARS" < "$RUNDIR/selfsign.template" > "$RUNDIR/selfsign.conf"
-
-# Schedule nginx reload to pick up new certificates
-crontab "$RUNDIR/crontab"
-# This will fork and allow us to continue
-crond
+for file in "$RUNDIR"/config/*.conf
+do
+  # Substitute only the variables listed above. Why?
+  # Nginx uses internal variables of the form $host. They must not be changed.
+  envsubst "$VARS" < "$file" > "/etc/nginx/conf.d/$(basename $file)"
+done
 
 # Without 'daemon off;' nginx will fork and the container will exit prematurely
 nginx -g "daemon off;"
