3c9e8cf1eae236745fce9be89e4e54d37a3ad912
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-12-02 23:07:23 -0500

    clear schedules first

--- backend/flow/api/parse/parse.go
+++ backend/flow/api/parse/parse.go
@@ -160,6 +160,9 @@ func HandleSchedule(state *state.State, w http.ResponseWriter, r *http.Request) 
 	}
 	defer tx.Rollback()
 
+	_, err = tx.Exec(
+		`DELETE FROM user_course_taken WHERE term_id = $1`, scheduleSummary.Term,
+	)
 	for _, classNumber := range scheduleSummary.ClassNumbers {
 		tag, err := tx.Exec(
 			`INSERT INTO user_schedule(user_id, section_id) `+
@@ -184,10 +187,10 @@ func HandleSchedule(state *state.State, w http.ResponseWriter, r *http.Request) 
 			return
 		}
 		_, err = tx.Exec(
-			`INSERT INTO user_course_taken(user_id, course_id) `+
-				`SELECT $1, course_id FROM course_section `+
-				`WHERE class_number = $2 AND term_id = $3`,
-			userId, classNumber, scheduleSummary.Term,
+			`INSERT INTO user_course_taken(user_id, term_id, course_id) `+
+				`SELECT $1, $2, course_id FROM course_section `+
+				`WHERE term_id = $2 AND class_number = $3`,
+			userId, scheduleSummary.Term, classNumber,
 		)
 	}
 
