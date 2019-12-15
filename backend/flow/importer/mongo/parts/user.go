acb4e280a545af3885add90aa26d9bf1990bb430
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-12-14 19:52:34 -0500

    be smart about user programs

--- backend/flow/importer/mongo/parts/user.go
+++ backend/flow/importer/mongo/parts/user.go
@@ -7,6 +7,7 @@ import (
 
 	"flow/common/db"
 	"flow/common/state"
+	"flow/common/util"
 	"flow/common/util/random"
 	"flow/importer/mongo/log"
 
@@ -23,7 +24,7 @@ type MongoUser struct {
 	ProgramName *string            `bson:"program_name"`
 	Email       *string            `bson:"email"`
 	Password    *string            `bson:"password"`
-	FBId        *string            `bson:"fbid"`
+	FbId        *string            `bson:"fbid"`
 }
 
 func readMongoUsers(rootPath string) []MongoUser {
@@ -42,6 +43,25 @@ func readMongoUsers(rootPath string) []MongoUser {
 		data = data[len(r):]
 	}
 	return users
+}
+
+var programNameMap = map[string]*string{
+	"Level: ":                        nil,
+	"Graduate unofficial transcript": nil,
+	"ComputerScience":                util.StringToPointer("Computer Science"),
+	"ity of Waterloo 200 University Ave. West Waterloo Ontario Canada N2L3G1":               nil,
+	"graduate Unofficial Transcript, Level: 1A, PostBaccalaureateDiplomainAccounting":       nil,
+	"ScienceandBusiness/EarthSciences":                                                      util.StringToPointer("Science and Business/Earth Sciences"),
+	"Penis Studies, Honours":                                                                nil,
+	"(DoubleDegree)BusinessAdministration(WLU)andMathematics(UW)":                           util.StringToPointer("(Double Degree) Business Administration (WLU) and Mathematics (UW)"),
+	"ComputerScience/BusinessOption":                                                        util.StringToPointer("Computer Science/Business Option"),
+	"Will Be Determined At Graduation Time":                                                 nil,
+	"bu Abah, Omar Wael":                                                                    nil,
+	"GeographyandEnvironmentalManagement":                                                   util.StringToPointer("Geography and Environmental Management"),
+	"Penis, Honours":                                                                        nil,
+	"Mathematics/FinancialAnalysis&RiskManagement-CharteredFinancialAnalyst Specialization": util.StringToPointer("Mathematics/Financial Analysis & Rist Management - Chartered Finanacial Analyst Specialization"),
+	"Sedra Smith, Honours":                                                                  nil,
+	"Chen Studies, Honours":                                                                 nil,
 }
 
 func ImportUsers(state *state.State, idMap *IdentifierMap) error {
@@ -66,12 +86,19 @@ func ImportUsers(state *state.State, idMap *IdentifierMap) error {
 		fullName := strings.TrimSpace(user.FirstName + " " + user.LastName)
 		idMap.User[user.Id] = i + 1
 
-		// If the program name is longer than 256 characters,
-		// it's almost certainly not actually a program name.
-		// We have some users with entire transcipts as "program names".
-		// We take the liberty of dropping such long strings here.
-		if user.ProgramName != nil && len(*user.ProgramName) > 256 {
-			user.ProgramName = nil
+		if user.ProgramName != nil {
+			programName := *user.ProgramName
+			// If the program name is longer than 256 characters,
+			// it's almost certainly not actually a program name.
+			// We have some users with entire transcipts as "program names".
+			// We take the liberty of dropping such long strings here.
+			if len(programName) > 256 {
+				user.ProgramName = nil
+			} else if val, found := programNameMap[programName]; found {
+				user.ProgramName = val
+			} else if idx := strings.IndexByte(programName, ','); idx != -1 {
+				user.ProgramName = util.StringToPointer(programName[:idx])
+			}
 		}
 
 		// Only add email users with valid email and password
@@ -81,9 +108,9 @@ func ImportUsers(state *state.State, idMap *IdentifierMap) error {
 			emailCredentials = append(emailCredentials, []interface{}{i + 1, user.Password})
 		}
 
-		if user.FBId != nil {
+		if user.FbId != nil {
 			preparedUsers[i] = []interface{}{secretId, fullName, user.ProgramName, user.Email, "facebook"}
-			fbCredentials = append(fbCredentials, []interface{}{i + 1, user.FBId})
+			fbCredentials = append(fbCredentials, []interface{}{i + 1, user.FbId})
 		}
 	}
 
