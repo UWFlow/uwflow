679ae7d38f3a0da03bf4c14f9dcc7d57e8421002
Dmytro Shynkevych <dm.shynk@gmail.com>
2020-01-31 16:15:04 -0500

    Prof mapping (#84)

---
+++ backend/flow/importer/mongo/data/prof.go
@@ -0,0 +1,9 @@
+package data
+
+type ProfRename struct {
+	OldCode string
+	OldName string
+	NewCode string
+	NewName string
+	Delete  bool
+}
--- backend/flow/importer/mongo/main.go
+++ backend/flow/importer/mongo/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"log"
 
 	"flow/common/state"
+	"flow/importer/mongo/log"
 	"flow/importer/mongo/parts"
 )
 
@@ -13,13 +13,14 @@ type ImportFunction func(*state.State, *parts.IdentifierMap) error
 func main() {
 	state, err := state.New(context.Background(), "mongo")
 	if err != nil {
-		log.Fatalf("Initialization failed: %v", err)
+		log.Error(err)
 	}
 
 	idMap := &parts.IdentifierMap{}
 	operations := []ImportFunction{
 		parts.ImportCourses,
 		parts.ImportCourseRequisites,
+		parts.ImportProfRenames,
 		parts.ImportProfs,
 		parts.ImportSections,
 		parts.ImportUsers,
@@ -30,7 +31,7 @@ func main() {
 	for _, operation := range operations {
 		err = operation(state, idMap)
 		if err != nil {
-			log.Fatalf("Import failed: %v", err)
+			log.Error(err)
 		}
 	}
 }
