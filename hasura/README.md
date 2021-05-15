fc8e6d5e2494d52cbbc3edee00363b95abdb385b
Edwin Zhang <edwinzhang64@gmail.com>
2021-05-15 09:06:09 -0600

    Add has_prereqs flag to course search index view (#117)

--- hasura/README.md
+++ hasura/README.md
@@ -56,3 +56,33 @@ For example, with curl:
 $ curl -H 'x-hasura-admin-secret:secretinprod' http://localhost:8080/v1/graphql -d @payload
 ```
 will submit the contents of the file `payload` and get the response.
+
+## Creating New Database Migrations
+
+The following steps are based on the [Hasura migration documentation](https://hasura.io/docs/latest/graphql/core/migrations/migrations-setup.html). We also assume the Hasura CLI is installed.
+
+1. Navigate to the `hasura` folder and create a migration:
+```sh 
+$ hasura migrate create "migration_name_here"
+```
+
+2. Update the generated `up.sql` and `down.sql` to perform the migration.
+
+  In general, migrations of normal tables are fairly straightforward. However, materialized views cannot be modified, so you will need drop the entire materialized view, as well as any associated functions and views, before recreating everything. However, it shouldn't be necessary to drop or recreate and indexes associated with the materialized view. The `course_search_prereqs` migration contains an example of how to do this.
+
+3. Update `metadata/tables.yaml` with any required metadata changes to the GraphQL API.
+
+4. Apply the migration (using the correct admin secret defined your `.env`):
+```sh 
+$ hasura migrate apply --admin-secret "HASURA_GRAPHQL_ADMIN_SECRET"
+```
+
+5. Reload the Hasura metadata:
+```sh 
+$ hasura metadata reload --admin-secret "HASURA_GRAPHQL_ADMIN_SECRET"
+```
+
+6. (Optional) To test if the migration rollback works, use the `--down 1` flag:
+```sh 
+$ hasura migrate apply --down 1 --admin-secret "HASURA_GRAPHQL_ADMIN_SECRET"
+```
---
+++ hasura/migrations/1621045852149_course_search_prereqs/down.sql
@@ -0,0 +1,108 @@
+-- Remove has_prereqs column from the course_search_index views
+
+-- DROP RELATED VIEWS AND FUNCTIONS
+DROP TRIGGER refresh_section_meeting ON section_meeting;
+
+DROP FUNCTION refresh_section_meeting_views;
+DROP FUNCTION search_courses;
+DROP FUNCTION search_profs;
+
+DROP VIEW course_search_index;
+DROP MATERIALIZED VIEW materialized.course_search_index;
+
+-- RECREATE PREVIOUS MATERIALIZED VIEW AND FUNCTIONS
+CREATE MATERIALIZED VIEW materialized.course_search_index AS
+SELECT
+  course.id                                   AS course_id,
+  course.code                                 AS code,
+  course.name                                 AS name,
+  ARRAY_TO_STRING(REGEXP_MATCHES(
+    course.code, '^(.+?)[0-9]'), '')          AS course_letters,
+  materialized.course_rating.filled_count     AS ratings,
+  materialized.course_rating.liked            AS liked,
+  materialized.course_rating.easy             AS easy,
+  materialized.course_rating.useful           AS useful,
+  COALESCE(ARRAY_AGG(DISTINCT course_section.term_id)
+    FILTER (WHERE course_section.term_id IS NOT NULL),
+    ARRAY[]::INT[])                           AS terms,
+  COALESCE(ARRAY_AGG(DISTINCT materialized.prof_teaches_course.prof_id)
+    FILTER (WHERE materialized.prof_teaches_course.prof_id IS NOT NULL),
+    ARRAY[]::INT[])                           AS prof_ids,
+  to_tsvector('simple', course.code) ||
+  to_tsvector('simple', course.name) ||
+  -- index course numbers to support queries where the course code is split
+  -- ie) the query "ECE 105" should match both "ECE" and "105" because
+  -- the frontend will translate the raw query to "ECE:* & 105:*"
+  to_tsvector('simple', ARRAY_TO_STRING(REGEXP_MATCHES(course.code,
+    '^[a-z|A-Z]+([0-9]+[a-z|A-Z]*)'), ''))    AS document
+FROM course
+  LEFT JOIN course_section ON course_section.course_id = course.id
+  LEFT JOIN materialized.prof_teaches_course ON materialized.prof_teaches_course.course_id = course.id
+  LEFT JOIN materialized.course_rating ON materialized.course_rating.course_id = course.id
+GROUP BY course.id, ratings, liked, easy, useful;
+
+CREATE VIEW course_search_index AS
+SELECT * FROM materialized.course_search_index;
+
+CREATE FUNCTION refresh_section_meeting_views()
+RETURNS TRIGGER AS $$
+  BEGIN
+    EXECUTE 'REFRESH MATERIALIZED VIEW materialized.course_rating;';
+    EXECUTE 'REFRESH MATERIALIZED VIEW materialized.prof_rating;';
+    EXECUTE 'REFRESH MATERIALIZED VIEW materialized.prof_teaches_course;';
+    EXECUTE 'REFRESH MATERIALIZED VIEW materialized.course_search_index;';
+    EXECUTE 'REFRESH MATERIALIZED VIEW materialized.prof_search_index;';
+    RETURN NULL;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION search_courses(query TEXT, code_only BOOLEAN)
+RETURNS SETOF course_search_index AS $$
+  BEGIN
+    IF code_only THEN
+      RETURN QUERY
+      SELECT * FROM course_search_index
+        WHERE course_letters ILIKE query
+      ORDER BY ratings DESC;
+    ELSE
+      RETURN QUERY
+      SELECT * FROM course_search_index
+        WHERE document @@ to_tsquery('simple', query)
+      UNION
+      SELECT DISTINCT * FROM (SELECT unnest(course_ids) AS course_id
+        FROM prof_search_index
+        WHERE document @@ to_tsquery('simple', query)) prof_courses
+        LEFT JOIN course_search_index USING (course_id)
+      ORDER BY ratings DESC;
+    END IF;
+  END
+$$ LANGUAGE plpgsql STABLE;
+
+CREATE FUNCTION search_profs(query TEXT, code_only BOOLEAN)
+RETURNS SETOF prof_search_index AS $$
+  BEGIN
+    IF code_only THEN
+      RETURN QUERY
+      SELECT DISTINCT * FROM (SELECT unnest(prof_ids) AS prof_id
+        FROM course_search_index
+        WHERE course_letters ILIKE query) course_profs
+        LEFT JOIN prof_search_index USING (prof_id)
+      ORDER BY ratings DESC;
+    ELSE
+      RETURN QUERY
+      SELECT * FROM prof_search_index
+        WHERE document @@ to_tsquery('simple', query)
+      UNION
+      SELECT DISTINCT * FROM (SELECT unnest(prof_ids) AS prof_id
+        FROM course_search_index
+        WHERE document @@ to_tsquery('simple', query)) course_profs
+        LEFT JOIN prof_search_index USING (prof_id)
+      ORDER BY ratings DESC;
+    END IF;
+  END;
+$$ LANGUAGE plpgsql STABLE;
+
+CREATE TRIGGER refresh_section_meeting
+AFTER INSERT OR UPDATE OR DELETE ON section_meeting
+FOR EACH STATEMENT
+EXECUTE PROCEDURE refresh_section_meeting_views();
