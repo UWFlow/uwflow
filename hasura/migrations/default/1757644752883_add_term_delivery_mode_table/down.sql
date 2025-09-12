406056b0427d374b694aad4409b2cc287207d631
Ando Deng <87212291+AD1938@users.noreply.github.com>
2025-09-12 09:40:25 -0400

    wip: add hasura migration for creating the new table (#140)

---
+++ hasura/migrations/default/1757644752883_add_term_delivery_mode_table/down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS public.idx_course_term_delivery_modes_delivery_mode;
+DROP INDEX IF EXISTS public.idx_course_term_delivery_modes_term_id;
+DROP INDEX IF EXISTS public.idx_course_term_delivery_modes_course_id;
+DROP TABLE IF EXISTS public.course_term_delivery_modes;
