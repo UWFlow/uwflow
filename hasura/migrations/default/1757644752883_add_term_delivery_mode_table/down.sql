7fc15ef1336b318c522d52452a15b4d2ddbe9718
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2025-09-27 15:30:32 -0400

    ran `hasura migrate squash --from 1559740220527`

--- hasura/migrations/default/1757644752883_add_term_delivery_mode_table/down.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX IF EXISTS public.idx_course_term_delivery_modes_delivery_mode;
-DROP INDEX IF EXISTS public.idx_course_term_delivery_modes_term_id;
-DROP INDEX IF EXISTS public.idx_course_term_delivery_modes_course_id;
-DROP TABLE IF EXISTS public.course_term_delivery_modes;
