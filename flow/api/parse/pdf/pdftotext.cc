f976dfd21fbd52f662925feb78d081fd19b938b7
Toyosatomimi no Miko <110693261+mikomikotaishi@users.noreply.github.com>
2025-12-06 18:07:50 -0500

    Various code cleanup on pdftotext

--- flow/api/parse/pdf/pdftotext.cc
+++ flow/api/parse/pdf/pdftotext.cc
@@ -1,40 +1,44 @@
 #include <cstring>
 #include <string>
+#include <vector>
 
 #include "poppler/cpp/poppler-document.h"
 #include "poppler/cpp/poppler-page.h"
 
-void donothing(const std::string &, void *) {}
+using String = std::string;
+template <typename T>
+using Vector = std::vector<T>;
+using Document = poppler::document;
 
 extern "C" {
-  const char *pdftotext(const char *data, int data_size)
-  {
-      static bool has_reset_error_function = false;
-      if (!has_reset_error_function) {
-        // Do not log errors from poppler to stderr
-        poppler::set_debug_error_function(donothing, nullptr);
-        has_reset_error_function = true;
-      }
+    [[nodiscard]]
+    const char* pdfToText(const char* data, int dataSize) noexcept {
+        static bool hasResetErrorFunction = false;
+        if (!hasResetErrorFunction) {
+            // Do not log errors from poppler to stderr
+            poppler::set_debug_error_function(
+                []([[maybe_unused]] const String& s, [[maybe_unused]] void* p) -> void {}, 
+                nullptr
+            );
+            hasResetErrorFunction = true;
+        }
 
-      const auto *doc = poppler::document::load_from_raw_data(data, data_size);
-      if (doc == nullptr) {
-        return nullptr;
-      }
-      const int N = doc->pages();
+        const Document* doc = Document::load_from_raw_data(data, dataSize);
+        if (!doc) {
+            return nullptr;
+        }
+        const int pageCount = doc->pages();
 
-      std::vector<char> contents[N];
-      int text_length = 0;
-      for (int i = 0; i < N; ++i) {
-        contents[i] = doc->create_page(i)->text().to_utf8();
-        text_length += contents[i].size();
-      }
+        String result;
+        for (int i = 0; i < pageCount; ++i) {
+            Vector<char> pageText = doc->create_page(i)->text().to_utf8();
+            result.append(pageText.begin(), pageText.end());
+        }
 
-      char *buffer = (char *)std::malloc(text_length + 1);
-      for (int i = 0, offset = 0; i < N; offset += contents[i].size(), ++i) {
-        std::memcpy(buffer + offset, contents[i].data(), contents[i].size());
-      }
-      buffer[text_length] = '\0';
+        char* buffer = (char*)std::malloc(result.length() + 1);
+        std::memcpy(buffer, result.data(), result.length());
+        buffer[result.length()] = '\0';
 
-      return buffer;
-  }
+        return buffer;
+    }
 }
--- flow/api/parse/pdf/pdftotext.go
+++ flow/api/parse/pdf/pdftotext.go
@@ -3,7 +3,7 @@ package pdf
 // #cgo CFLAGS: -O2 -Wall -I/usr/include/poppler/cpp
 // #cgo LDFLAGS: -lpoppler-cpp
 // #include <stdlib.h>
-// const char *pdftotext(const char *data, int data_size);
+// const char *pdfToText(const char* data, int data_size);
 import "C"
 import (
 	"errors"
@@ -12,12 +12,12 @@ import (
 
 func ToText(data []byte) (string, error) {
 	// Is this safe? Kind of: `data`, a []byte is a continguous array in Go,
-	// so we can safely point a C-land (const char *) to it,
+	// so we can safely point a C-land (const char*) to it,
 	// *provided* that C code does not attempt to find the end of the string,
 	// as []byte need not be zero-terminated.
-	// This is true for us, as C.pdftotext treats its first argument as bytes.
+	// This is true for us, as C.pdfToText treats its first argument as bytes.
 	cData := (*C.char)(unsafe.Pointer(&data[0]))
-	result := C.pdftotext(cData, C.int(len(data)))
+	result := C.pdfToText(cData, C.int(len(data)))
 	if result != nil {
 		converted := C.GoString(result)
 		C.free(unsafe.Pointer(result))
