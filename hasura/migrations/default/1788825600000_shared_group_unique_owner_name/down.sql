efe61f090eb58f64fcf77a3ef4513b9420712faf
jerryzhou196 <jerry.zhou@uwaterloo.ca>
2026-09-08 11:12:23 -0600

    Fold group name uniqueness into shared classes migration

--- hasura/migrations/default/1788825600000_shared_group_unique_owner_name/down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.shared_group
-  DROP CONSTRAINT shared_group_created_by_name_key;
--- hasura/migrations/default/1788825600000_shared_group_unique_owner_name/up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.shared_group
-  ADD CONSTRAINT shared_group_created_by_name_key UNIQUE (created_by, name);
